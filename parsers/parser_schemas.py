e5270fe91cef9c874f878e23bed806e1424a1678
gityuri1000 <yuriforandroid@gmail.com>
2024-04-07 22:20:53 +0300

    Добавлено описание Pydantic и Enum

--- parsers/parser_schemas.py
+++ parsers/parser_schemas.py
@@ -3,12 +3,40 @@ from pydantic import BaseModel
 from enum import Enum
 
 class WebsiteCategoryURL(BaseModel):
+    """
+    Pydantic. Класс для валидации ссылок на категории сайта.
+
+    Структура:
+
+    url: str;
+
+    """
+
     url: str
 
 class WebsiteCategories(BaseModel):
+    """
+    Pydantic. Класс для валидации списка url-категорий сайта.
+
+    Структура:
+
+    categories: Sequence[WebsiteCategoryURL];
+
+    """
+
+
     categories: Sequence[WebsiteCategoryURL]
 
 class ItemURL(BaseModel):
+    """
+    Pydantic. Валидация ссылки на товар.
+
+    Структура:
+
+    item_url: str;
+
+    """
+
     item_url: str
 
     def __hash__(self) -> int:
@@ -19,6 +47,14 @@ class WebsiteItemData(BaseModel):
     Pydantic. Объекты данного класса - информация 
     о вещи, находящейся на сайте. Далее помещается
     в таблицу parsed_item_table.
+
+    Структура:
+
+    item_name: str;
+    item_url: str;
+    shop: str;
+    current_price: Union[int, float];
+    is_active: int;
 
     """
 
@@ -33,6 +69,12 @@ class WebsiteURL(Enum):
     Enum. Содержит ссылки на сайты,
     с которых собирается информация о товарах.
 
+    Значения:
+
+    Rogov = "rogovshop.ru";
+    RedSeptember = "redseptemberdesign.com";
+    Fable = "https://fablestore.ru";
+
     """
 
     Rogov = "rogovshop.ru"
@@ -43,11 +85,15 @@ class ShopName(Enum):
     """
     Enum. Содержит названия магазинов,
     с которых собирается инфомация о товарах.
-    
+
+    Значения:
+
+    Rogov = "Rogov";
+    RedSeptember = "Red September";
+    Fable = "Fable";
+
     """
 
     Rogov = "Rogov"
     RedSeptember = "Red September"
     Fable = "Fable"
-
-
